98fd4679b670a4045a34ca424ccebcc2a5d285c9
mebdrey <mebdelrey@gmail.com>
2025-09-30 16:25:52 -0300

    ajustes tecnicos

--- gestaoagro/frontend/web/src/app/(public)/cadastrar/page.jsx
+++ gestaoagro/frontend/web/src/app/(public)/cadastrar/page.jsx
@@ -7,27 +7,16 @@ export default function CadastrarPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
+        <img src="/placeholder.svg" alt="Image" className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
       </div>
-      <div className="flex flex-col gap-4 p-6 md:p-10 relative"> {/* Adicionei relative aqui */}
+      <div className="flex flex-col gap-4 p-6 md:p-10 relative">
         {/* Imagem de fundo fora do container do logo */}
-        <img
-          src="/img/seila.svg"
-          className='absolute top-0 left-0 w-[800px] h-[600px] z-0 -ml-19.5 -mt-10  dark:brightness-[0.0]'
-          alt="Background"
-        />
+        <img src="/img/seila.svg" className='absolute top-0 left-0 w-[800px] h-[600px] z-0 -ml-19.5 -mt-10  dark:brightness-[0.0]' alt="Background"/>
 
         <div className="flex items-center relative z-10"> {/* z-10 para ficar acima do fundo */}
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex items-center justify-center rounded-md">
-              <img
-                src="/img/logoRuralTech.svg"
-                className='w-80 h-20 dark:brightness-[0.9]'
-                alt="RuralTech Logo"
-              />
+              <img src="/img/logoRuralTech.svg" className='w-80 h-20 dark:brightness-[0.9]' alt="RuralTech Logo"/>
             </div>
           </a>
           <div className="flex-1"></div>
@@ -42,44 +31,3 @@ export default function CadastrarPage() {
     </div>
   );
 }
-
-// import { GalleryVerticalEnd } from "lucide-react"
-
-// import { ThemeToggle } from "@/components/toggleSwitchTema";
-// import { CadastrarForm } from "@/components/cadastrar-form"
-
-// export default function CadastrarPage() {
-//   return (
-//     <div className="grid min-h-svh lg:grid-cols-2">
-//       <div className="flex flex-col gap-4 p-6 md:p-10 relative"> {/* Adicionei relative aqui */}
-//         {/* Imagem de fundo fora do container do logo */}
-//         <img
-//           src="/img/seila.svg"
-//           className='absolute top-0 left-0 w-[800px] h-[600px] z-0 -ml-30 -mt-10  dark:brightness-[0.0]'
-//           alt="Background"
-//         />
-
-//         <div className="flex items-center relative z-10"> {/* z-10 para ficar acima do fundo */}
-//           <a href="#" className="flex items-center gap-2 font-medium">
-//             <div className="flex items-center justify-center rounded-md">
-//               <img
-//                 src="/img/logoRuralTech.svg"
-//                 className='w-80 h-20 dark:brightness-[0.9]'
-//                 alt="RuralTech Logo"
-//               />
-//             </div>
-//           </a>
-//           <div className="flex-1"></div>
-//           <ThemeToggle />
-//         </div>
-//         <div className="flex flex-1 items-center justify-center">
-//           <div className="w-full max-w-xs">
-//             <CadastrarForm />
-//           </div>
-//         </div>
-//       </div>
-
-//     </div>
-//   );
-// }
-
--- gestaoagro/frontend/web/src/app/(public)/login/page.jsx
+++ gestaoagro/frontend/web/src/app/(public)/login/page.jsx
@@ -6,22 +6,14 @@ export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex flex-col gap-4 p-6 md:p-10 relative"> {/* Adicionei relative aqui */}
+        <div className="flex flex-col gap-4 p-6 md:p-10 relative">
           {/* Imagem de fundo fora do container do logo */}
-          <img
-            src="/img/seila.svg"
-            className='absolute top-0 left-0 w-[800px] h-[600px] z-0 -ml-30 -mt-10  dark:brightness-[0.0]'
-            alt="Background"
-          />
+          <img src="/img/seila.svg" className='absolute top-0 left-0 w-[800px] h-[600px] z-0 -ml-30 -mt-10  dark:brightness-[0.0]' alt="Background"/>
 
-          <div className="flex items-center relative z-10"> {/* z-10 para ficar acima do fundo */}
+          <div className="flex items-center relative z-10">
             <a href="#" className="flex items-center gap-2 font-medium">
               <div className="flex items-center justify-center rounded-md">
-                <img
-                  src="/img/logoRuralTech.svg"
-                  className='w-80 h-20 dark:brightness-[0.9]'
-                  alt="RuralTech Logo"
-                />
+                <img src="/img/logoRuralTech.svg" className='w-80 h-20 dark:brightness-[0.9]' alt="RuralTech Logo"/>
               </div>
             </a>
             <div className="flex-1"></div>
