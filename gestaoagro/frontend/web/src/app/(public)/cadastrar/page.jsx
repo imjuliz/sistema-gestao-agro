06efbdf353261b07a6f89fe7d93f4f344f0eeda1
mebdrey <mebdelrey@gmail.com>
2025-09-30 16:27:55 -0300

    novos ajustes técnicos

--- gestaoagro/frontend/web/src/app/(public)/cadastrar/page.jsx
+++ gestaoagro/frontend/web/src/app/(public)/cadastrar/page.jsx
@@ -10,10 +10,9 @@ export default function CadastrarPage() {
         <img src="/placeholder.svg" alt="Image" className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10 relative">
-        {/* Imagem de fundo fora do container do logo */}
         <img src="/img/seila.svg" className='absolute top-0 left-0 w-[800px] h-[600px] z-0 -ml-19.5 -mt-10  dark:brightness-[0.0]' alt="Background"/>
 
-        <div className="flex items-center relative z-10"> {/* z-10 para ficar acima do fundo */}
+        <div className="flex items-center relative z-10"> 
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex items-center justify-center rounded-md">
               <img src="/img/logoRuralTech.svg" className='w-80 h-20 dark:brightness-[0.9]' alt="RuralTech Logo"/>
--- gestaoagro/frontend/web/src/components/toggleSwitchTema.jsx
+++ gestaoagro/frontend/web/src/components/toggleSwitchTema.jsx
@@ -7,20 +7,15 @@ export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  useEffect(() => { setMounted(true);}, []);
 
-  if (!mounted) {
-    return null; // Evita hidratação mismatch
-  }
+  if (!mounted) { return null; }
 
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="p-2 rounded-lg bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors"
-      aria-label="Alternar tema"
-    >
+      aria-label="Alternar tema" >
       {theme === "dark" ? "🌙" : "☀️"}
     </button>
   );
